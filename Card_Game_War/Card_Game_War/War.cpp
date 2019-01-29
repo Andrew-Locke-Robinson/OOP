d2318be653b4fd98dbf9d1f926c2b0169a62580c
Andrew-Locke-Robinson <44908942+Andrew-Locke-Robinson@users.noreply.github.com>
2019-01-29 11:16:19 -0500

    Implement initialize player decks to War class.

--- Card_Game_War/Card_Game_War/War.cpp
+++ Card_Game_War/Card_Game_War/War.cpp
@@ -1,5 +1,6 @@
 #include "War.hpp"
 #include <random>
+#include <iostream>
 
 
 
@@ -28,7 +29,28 @@ void War::InitializePlayerDecks()
 	}
 	for (int i = 0; i < 52; ++i)
 	{
-		
+		RandomIntArraySwap(random_list, i);
 	}
+	for (int i = 0; i < 26; ++i)
+	{
+		{
+			Card newCard(static_cast<Card::Suit>(random_list[i] / 13), static_cast<Card::Rank>(random_list[i] % 13));
+			player1_deck_.AddCard(newCard);
+		}
+		{
+			Card newCard(static_cast<Card::Suit>(random_list[i+26] / 13), static_cast<Card::Rank>(random_list[i+26] % 13));
+			player2_deck_.AddCard(newCard);
+		}
+	}
+}
+
+void War::RandomIntArraySwap(int * int_list, int position)
+{
+	int random_position = rand() % 52;
+	int temp = int_list[position];
+	int_list[position] = int_list[random_position];
+	int_list[random_position] = temp;
+
+	return;
 }
 
--- Card_Game_War/Card_Game_War/main.cpp
+++ Card_Game_War/Card_Game_War/main.cpp
@@ -10,14 +10,7 @@ int main()
 
 	War game;
 
-	for (int s = 0; s < 4; ++s)
-	{
-		for (int r = 0; r < 13; r++)
-		{
-			Card new_card(static_cast<Card::Suit>(s), static_cast<Card::Rank> (r));
-			main_deck.AddCard(new_card);
-		}
-	}
+	game.InitializePlayerDecks();
 
 	int x;
 	std::cin >> x;
